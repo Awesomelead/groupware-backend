4b50bb1b4a93126b1d5c97174fa2f711c42ed06b
Go Yeongmin <ko07073@gmail.com>
2025-09-20 23:50:50 +0900

    feat: 초기 파일 구조 설정

--- src/main/java/kr/co/awesomelead/groupware_backend/GroupwareBackendApplication.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/GroupwareBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GroupwareBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GroupwareBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GroupwareBackendApplication.class, args);
+    }
 
 }

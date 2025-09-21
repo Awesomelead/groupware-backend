a08f1070c97562e74dd4a176358a8279070bb293
Go Yeongmin <ko07073@gmail.com>
2025-09-21 17:04:24 +0900

    test: 스타일 체크 검증용 정상적인 포맷 코드 추가

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/test/controller/StyleTestController.java
@@ -0,0 +1,15 @@
+package kr.co.awesomelead.groupware_backend.domain.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StyleTestController {
+
+    private String message = "안녕하세요";
+
+    @GetMapping("/test")
+    public String test() {
+        return message;
+    }
+}

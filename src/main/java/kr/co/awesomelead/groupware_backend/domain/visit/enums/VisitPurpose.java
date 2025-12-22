d7f7b6ced5cd4fdd601a12665bdb6ec8022f6977
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-12-22 17:05:12 +0900

    feat: implement Department and User entities with relationships and fields

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/enums/VisitPurpose.java
@@ -0,0 +1,18 @@
+package kr.co.awesomelead.groupware_backend.domain.visit.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum VisitPurpose {
+
+    CUSTOMER_INSPECTION("고객 검수"),
+    GOODS_DELIVERY("물품 납품"),
+    FACILITY_CONSTRUCTION("시설공사"),
+    LOGISTICS("입출고"),
+    MEETING("미팅"),
+    OTHER("기타");
+
+    private final String description; // 한글 설명
+}

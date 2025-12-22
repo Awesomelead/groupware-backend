e99edb69b72fb311b889e7232955ed8e51063a1f
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-12-22 17:04:22 +0900

    feat: implement Companion, VisitInfo, and Visitor entities with relationships

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/entity/Companion.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/entity/Companion.java
@@ -1,3 +1,37 @@
 package kr.co.awesomelead.groupware_backend.domain.visit.entity;
 
-public class Companion {}
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Companion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 10)
+    private String name; // 동행자 이름
+
+    @Column(length = 50)
+    private String hostCompany; // 동행자 회사
+
+    @Column(nullable = false, length = 11)
+    private String phoneNumber; // 동행자 전화번호
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "visit_info_id")
+    private VisitInfo visitInfo; // 동행한 방문 정보
+
+
+}
--- src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/entity/Visitor.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/entity/Visitor.java
@@ -1,3 +1,36 @@
 package kr.co.awesomelead.groupware_backend.domain.visit.entity;
 
-public class Visitor {}
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Visitor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 10)
+    private String name; // 방문자 이름
+
+    @Column(nullable = false, length = 11)
+    private String phoneNumber; // 방문자 전화번호
+
+    @Column(nullable = false, columnDefinition = "CHAR(4)")
+    private String password; // 방문자 비밀번호 (4자리 숫자)
+
+    @OneToMany(mappedBy = "visitor")
+    @JsonManagedReference
+    private List<VisitInfo> visitInfos = new ArrayList<>(); // 방문 기록들
+}

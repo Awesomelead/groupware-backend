8baf5cf749fe2d6e2ad8a1a403fb9c1c2da6458c
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-12-22 17:05:17 +0900

    feat: add VisitPurpose enum with various visit purposes and descriptions

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/requesthistory/enums/RequestType.java
@@ -0,0 +1,14 @@
+package kr.co.awesomelead.groupware_backend.domain.requesthistory.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum RequestType {
+
+    EMPLOYMENT_CERTIFICATE("재직증명서"), // 재직 중임을 증명
+    CAREER_CERTIFICATE("경력증명서");     // 과거 경력 사항을 증명
+
+    private final String description;
+}

837f601b71e9eb1f7b3f029c6d0edbe9d60e71d5
Go Yeongmin <ko07073@gmail.com>
2025-12-24 01:12:21 +0900

    feat: 카카오채널 관리 - 인증요청 구현

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/config/AligoConfig.java
@@ -0,0 +1,35 @@
+package kr.co.awesomelead.groupware_backend.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "aligo")
+@Getter
+@Setter
+public class AligoConfig {
+
+    private Api api;
+    private Kakao kakao;
+
+    @Getter
+    @Setter
+    public static class Api {
+
+        private String key;
+        private String userid;
+    }
+
+    @Getter
+    @Setter
+    public static class Kakao {
+
+        private String plusid;
+        private String adminPhone;
+        private String senderkey;
+        private String authUrl;
+        private String sendUrl;
+    }
+}

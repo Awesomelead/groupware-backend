815ca2308b3ee949b91aee3517d049a1f81f8d12
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-12-23 21:56:49 +0900

    feat: add autogenerated ID field to Message and MessageAttachment entities

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/message/entity/Message.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/message/entity/Message.java
@@ -5,6 +5,8 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
@@ -27,6 +29,7 @@ import lombok.Setter;
 public class Message {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/kr/co/awesomelead/groupware_backend/domain/message/entity/MessageAttachment.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/message/entity/MessageAttachment.java
@@ -3,6 +3,8 @@ package kr.co.awesomelead.groupware_backend.domain.message.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -17,6 +19,7 @@ import lombok.Setter;
 public class MessageAttachment {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)

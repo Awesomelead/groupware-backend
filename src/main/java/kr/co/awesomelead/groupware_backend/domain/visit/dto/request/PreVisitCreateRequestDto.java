e3c6033a982f75e8ede603cef2c6dd4142d1de0c
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-12-26 22:04:13 +0900

    feat: implement on-site and pre visit request dto

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/dto/request/PreVisitCreateRequestDto.java
@@ -0,0 +1,39 @@
+package kr.co.awesomelead.groupware_backend.domain.visit.dto.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.List;
+import kr.co.awesomelead.groupware_backend.domain.visit.enums.VisitPurpose;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PreVisitCreateRequestDto {
+
+    @NotNull
+    private Long hostUserId; // 담당자 ID
+
+    @NotNull
+    @Valid
+    private VisitorRequestDto visitor;
+
+    @NotBlank
+    private String hostCompany;
+    @NotNull
+    private VisitPurpose purpose;
+    private String carNumber;
+
+    @NotNull
+    private LocalDateTime visitStartDate;
+    @NotNull
+    private LocalDateTime visitEndDate;
+
+    @Valid
+    private List<CompanionRequestDto> companions;
+
+}

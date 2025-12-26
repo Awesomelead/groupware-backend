568cc9434cfd0b25284ec62edaa9bceda6bb3778
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-12-26 22:01:41 +0900

    feat: update Visitor entity and add VisitorRequestDto for visitor data handling

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/dto/request/VisitorRequestDto.java
@@ -0,0 +1,30 @@
+package kr.co.awesomelead.groupware_backend.domain.visit.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class VisitorRequestDto {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Pattern(regexp = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$", message = "올바른 전화번호 형식이 아닙니다.")
+    // 하이픈 제외
+    private String phoneNumber;
+
+    @NotBlank
+    @Size(min = 4, max = 4, message = "비밀번호는 4자리여야 합니다.")
+    private String password;
+
+    @NotBlank
+    private String visitorCompany;
+
+}
--- src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/entity/Visitor.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/entity/Visitor.java
@@ -1,19 +1,16 @@
 package kr.co.awesomelead.groupware_backend.domain.visit.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 @Setter
 @Getter
@@ -35,5 +32,5 @@ public class Visitor {
 
     @OneToMany(mappedBy = "visitor")
     @JsonManagedReference
-    private List<VisitInfo> visitInfos = new ArrayList<>(); // 방문 기록들
+    private List<Visit> visitInfos = new ArrayList<>(); // 방문 기록들
 }

2a6334c2f12b63bce1e02739d9dcb111352093ec
Go Yeongmin <ko07073@gmail.com>
2025-12-27 00:55:12 +0900

    refactor: test application.yml에서 환경변수 제거

--- src/test/java/kr/co/awesomelead/groupware_backend/GroupwareBackendApplicationTests.java
+++ src/test/java/kr/co/awesomelead/groupware_backend/GroupwareBackendApplicationTests.java
@@ -2,10 +2,13 @@ package kr.co.awesomelead.groupware_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GroupwareBackendApplicationTests {
 
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+    }
 }

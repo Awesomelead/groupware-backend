f6606038b77709c542d9fff75f20fc4f2bae8e3a
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-12-29 02:06:29 +0900

    feat: configure ObjectMapper for Java 8 time support and add MapStruct dependency

--- src/main/java/kr/co/awesomelead/groupware_backend/config/AppConfig.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/config/AppConfig.java
@@ -1,7 +1,8 @@
 package kr.co.awesomelead.groupware_backend.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -19,7 +20,11 @@ public class AppConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return mapper;
     }
 
     @Bean

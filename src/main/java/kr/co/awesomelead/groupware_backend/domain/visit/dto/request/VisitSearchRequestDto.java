90e400efb0bfb7fbd127506a9b68af4ad8564028
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-12-30 16:53:25 +0900

    refactor: rename VisitorSearchRequestDto to VisitSearchRequestDto for consistency

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/dto/request/VisitSearchRequestDto.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/dto/request/VisitSearchRequestDto.java
@@ -3,7 +3,6 @@ package kr.co.awesomelead.groupware_backend.domain.visit.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,9 +10,10 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class VisitorSearchRequestDto {
+public class VisitSearchRequestDto {
 
-    @NotBlank private String name;
+    @NotBlank
+    private String name;
 
     @NotBlank
     @Pattern(regexp = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$", message = "올바른 전화번호 형식이 아닙니다.")

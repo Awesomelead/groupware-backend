639be46e9ac851fc7b4b731f5f6ba160b4530194
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-12-30 16:53:13 +0900

    feat: add method to find visits by visitor in VisitRepository

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/repository/VisitRepository.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/repository/VisitRepository.java
@@ -1,7 +1,11 @@
 package kr.co.awesomelead.groupware_backend.domain.visit.repository;
 
+import java.util.List;
 import kr.co.awesomelead.groupware_backend.domain.visit.entity.Visit;
-
+import kr.co.awesomelead.groupware_backend.domain.visit.entity.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VisitRepository extends JpaRepository<Visit, Long> {}
+public interface VisitRepository extends JpaRepository<Visit, Long> {
+
+    List<Visit> findByVisitor(Visitor visitor);
+}

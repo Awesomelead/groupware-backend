bcbb36df3d9909ec6537adb67ce32e0258517a7b
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-12-31 17:54:01 +0900

    feat: add NoArgsConstructor and Builder annotations to EduReport DTOs

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/education/dto/response/EduReportDetailDto.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/education/dto/response/EduReportDetailDto.java
@@ -5,11 +5,14 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class EduReportDetailDto {
 
     private Long id;
--- src/main/java/kr/co/awesomelead/groupware_backend/domain/education/dto/response/EduReportSummaryDto.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/education/dto/response/EduReportSummaryDto.java
@@ -3,12 +3,14 @@ package kr.co.awesomelead.groupware_backend.domain.education.dto.response;
 import java.time.LocalDate;
 import kr.co.awesomelead.groupware_backend.domain.education.enums.EduType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class EduReportSummaryDto {
 
     private Long id;

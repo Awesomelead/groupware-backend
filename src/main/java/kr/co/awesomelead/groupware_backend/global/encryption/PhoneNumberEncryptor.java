c5e0a5b74ce6e1ba8f4f965558d4790059fbc32f
Go Yeongmin <ko07073@gmail.com>
2025-12-31 18:30:54 +0900

    feat: 전화번호 암호화 Converter

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/global/encryption/PhoneNumberEncryptor.java
@@ -0,0 +1,27 @@
+package kr.co.awesomelead.groupware_backend.global.encryption;
+
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Converter
+@Component
+@RequiredArgsConstructor
+public class PhoneNumberEncryptor implements AttributeConverter<String, String> {
+
+    private final AESEncryptor aesEncryptor;
+
+    // Entity → DB 저장 시 호출 (암호화)
+    @Override
+    public String convertToDatabaseColumn(String attribute) {
+        return aesEncryptor.encrypt(attribute);
+    }
+
+    // DB → Entity 조회 시 호출 (복호화)
+    @Override
+    public String convertToEntityAttribute(String dbData) {
+        return aesEncryptor.decrypt(dbData);
+    }
+}

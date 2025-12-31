6c715b679b1f5f6622e4fb18c9c7a24ae351709d
Go Yeongmin <ko07073@gmail.com>
2025-12-31 19:17:39 +0900

    refactor: 주민등록번호 -> 생년월일 수동 변환 메서드를 Setter 오버라이딩해서 변경

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/user/entity/User.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/user/entity/User.java
@@ -2,7 +2,6 @@ package kr.co.awesomelead.groupware_backend.domain.user.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
@@ -20,7 +19,12 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import kr.co.awesomelead.groupware_backend.domain.annualleave.entity.AnnualLeave;
 import kr.co.awesomelead.groupware_backend.domain.checksheet.entity.CheckSheet;
 import kr.co.awesomelead.groupware_backend.domain.department.entity.Department;
@@ -34,16 +38,8 @@ import kr.co.awesomelead.groupware_backend.domain.user.enums.Status;
 import kr.co.awesomelead.groupware_backend.domain.visit.entity.Visit;
 import kr.co.awesomelead.groupware_backend.global.encryption.PhoneNumberEncryptor;
 import kr.co.awesomelead.groupware_backend.global.encryption.RegistrationNumberEncryptor;
-
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Entity
 @Getter
@@ -153,35 +149,38 @@ public class User {
         return this.authorities.contains(authority);
     }
 
-    public void calculateBirthDateFromRegistrationNumber() {
-        if (this.registrationNumber == null || this.registrationNumber.length() < 7) {
-            return;
-        }
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
 
+        // 암호화되기 전 원본 값으로 생년월일 계산
+        if (registrationNumber != null && registrationNumber.length() >= 7) {
+            this.birthDate = calculateBirthDate(registrationNumber);
+        }
+    }
+
+    private LocalDate calculateBirthDate(String regNum) {
         // 앞 6자리 추출 (YYMMDD)
-        String birthPart = this.registrationNumber.substring(0, 6);
+        String birthPart = regNum.substring(0, 6);
 
         // 뒤 첫 번째 자리 추출 (성별/세기 구분자)
-        char genderDigit =
-                this.registrationNumber.contains("-")
-                        ? this.registrationNumber.charAt(7)
-                        : this.registrationNumber.charAt(6);
+        char genderDigit = regNum.contains("-")
+            ? regNum.charAt(7)
+            : regNum.charAt(6);
 
         // 세기 판단
         String century;
-        if (genderDigit == '1' || genderDigit == '2' || genderDigit == '5' || genderDigit == '6') {
+        if (genderDigit == '1' || genderDigit == '2' ||
+            genderDigit == '5' || genderDigit == '6') {
             century = "19";
-        } else if (genderDigit == '3'
-                || genderDigit == '4'
-                || genderDigit == '7'
-                || genderDigit == '8') {
+        } else if (genderDigit == '3' || genderDigit == '4' ||
+            genderDigit == '7' || genderDigit == '8') {
             century = "20";
         } else {
-            century = "20"; // 기본값 (보통 2000년대생)
+            century = "20"; // 기본값
         }
 
-        // LocalDate로 변환하여 set
+        // LocalDate로 변환
         String fullDate = century + birthPart;
-        this.birthDate = LocalDate.parse(fullDate, DateTimeFormatter.ofPattern("yyyyMMdd"));
+        return LocalDate.parse(fullDate, DateTimeFormatter.ofPattern("yyyyMMdd"));
     }
 }

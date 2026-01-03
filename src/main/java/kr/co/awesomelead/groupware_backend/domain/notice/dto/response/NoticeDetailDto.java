96cc6c1231f3a992e19485ac37d7dd8780e6c101
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-04 03:38:09 +0900

    feat: 공지 상세조회 DTO 구현

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/notice/dto/response/NoticeDetailDto.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/notice/dto/response/NoticeDetailDto.java
@@ -1,9 +1,12 @@
 package kr.co.awesomelead.groupware_backend.domain.notice.dto.response;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
@@ -11,5 +14,26 @@ import lombok.NoArgsConstructor;
 @Builder
 public class NoticeDetailDto {
 
+    private Long id;
+    private String title;
+    private String content;
+    private String authorName;
+    private LocalDateTime updatedDate;
+    private int viewCount;
+
+    private List<AttachmentResponse> attachments;
+
+    @Getter
+    @Setter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class AttachmentResponse {
+
+        private Long id;
+        private String originalFileName;
+        private long fileSize;
+        private String viewUrl; // S3에서 바로 열기 위한 URL
+    }
 
 }

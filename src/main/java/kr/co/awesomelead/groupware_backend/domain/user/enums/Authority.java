349344a3b4dac513b58f8e13db09e8b8ebba1b77
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-03 22:58:31 +0900

    feat: Authority에 공지 작성 권한 추가

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/user/enums/Authority.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/user/enums/Authority.java
@@ -3,4 +3,5 @@ package kr.co.awesomelead.groupware_backend.domain.user.enums;
 public enum Authority {
     WRITE_MESSAGE, // 메세지 작성 권한
     WRITE_EDUCATION, // 교육 작성 권한
+    WRITE_NOTICE // 공지 작성 권한
 }

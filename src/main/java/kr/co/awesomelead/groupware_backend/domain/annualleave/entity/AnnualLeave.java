75f132b48905927e14fd30f6f6f99f50dc661983
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-07 19:21:14 +0900

    refactor: 실제 연차엑셀 데이터와 통일을 위해 월차와 이월 월차 필드 추가

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/annualleave/entity/AnnualLeave.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/annualleave/entity/AnnualLeave.java
@@ -1,7 +1,6 @@
 package kr.co.awesomelead.groupware_backend.domain.annualleave.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -9,25 +8,28 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
-
+import java.time.LocalDate;
 import kr.co.awesomelead.groupware_backend.domain.user.entity.User;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.Date;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnnualLeave {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private int total;
-    private int used;
-    private int remain;
-    private Date updateDate;
+    private Double total; // 발생 연차
+    private Double monthlyLeave; // 월차
+    private Double carriedOver; //이월 월차
+    private Double used; // 사용
+    private Double remain; //잔여일
+    private LocalDate updateDate; // 수정일
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")

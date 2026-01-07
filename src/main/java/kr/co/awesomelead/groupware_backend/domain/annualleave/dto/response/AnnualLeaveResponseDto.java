3bad0374135a6fd6394c29ad125e23c72b1d5da7
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-07 19:24:41 +0900

    feat: 개인용 연차 조회 DTO 작성

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/annualleave/dto/response/AnnualLeaveResponseDto.java
@@ -0,0 +1,22 @@
+package kr.co.awesomelead.groupware_backend.domain.annualleave.dto.response;
+
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnnualLeaveResponseDto {
+
+    private Double total; // 발생 연차
+    private Double monthlyLeave; // 월차
+    private Double carriedOver; //이월 월차
+    private Double used; // 사용
+    private Double remain; //잔여일
+    private LocalDate updateDate; // 수정일
+
+}

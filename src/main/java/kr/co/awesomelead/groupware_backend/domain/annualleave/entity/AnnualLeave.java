c999c1e3a443fbc7171af2b6f5695849d5c6fc83
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-08 03:42:03 +0900

    feat: AnnualLeave 엔티티에 Builder 패턴 추가

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/annualleave/entity/AnnualLeave.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/annualleave/entity/AnnualLeave.java
@@ -11,11 +11,13 @@ import jakarta.persistence.OneToOne;
 import java.time.LocalDate;
 import kr.co.awesomelead.groupware_backend.domain.user.entity.User;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AnnualLeave {

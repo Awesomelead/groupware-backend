e7f7b203d57aaea97fa8530ffcaff551cacb2390
Go Yeongmin <ko07073@gmail.com>
2026-01-10 18:29:47 +0900

    feat: 계정 삭제 requestDto 생성

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/auth/dto/request/DeleteUserRequestDto.java
@@ -0,0 +1,18 @@
+package kr.co.awesomelead.groupware_backend.domain.auth.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Schema(description = "[테스트] 계정 삭제")
+public class DeleteUserRequestDto {
+
+    @Schema(description = "이메일", example = "test@example.com", required = true)
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "유효한 이메일 형식이 아닙니다.")
+    private String email;
+}

1e8a9edf81ffa157cab72931ca916b6cf4148945
Go Yeongmin <ko07073@gmail.com>
2026-01-11 17:17:52 +0900

    feat: 로그인 결과 dto 생성

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/auth/dto/response/LoginiResultDto.java
@@ -0,0 +1,13 @@
+package kr.co.awesomelead.groupware_backend.domain.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginiResultDto {
+
+    private LoginResponseDto loginResponse;
+    private String refreshToken;
+
+}

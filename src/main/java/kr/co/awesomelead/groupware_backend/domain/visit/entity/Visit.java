b18878d2b3d3e4882ffebd48efd7fe2e437ecf8f
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-11 18:21:09 +0900

    refactor: 회사도 String에서 Company로 수정

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/entity/Visit.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/entity/Visit.java
@@ -2,7 +2,6 @@ package kr.co.awesomelead.groupware_backend.domain.visit.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -15,21 +14,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import kr.co.awesomelead.groupware_backend.domain.department.enums.Company;
 import kr.co.awesomelead.groupware_backend.domain.user.entity.User;
 import kr.co.awesomelead.groupware_backend.domain.visit.enums.VisitPurpose;
 import kr.co.awesomelead.groupware_backend.domain.visit.enums.VisitType;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Setter
 @Getter
@@ -44,7 +41,7 @@ public class Visit {
     private Long id;
 
     @Column(nullable = false, length = 50)
-    private String hostCompany; // 내방객 방문 회사
+    private Company hostCompany; // 내방객 방문 회사
 
     @Column(length = 50)
     private String visitorCompany; // 내방객 소속 회사

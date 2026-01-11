6efce05fdfb9e8385dea46563b85a905d7af1ae9
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-11 15:27:15 +0900

    refactor: Payslip 엔티티 빌더패턴 추가

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/payslip/entity/Payslip.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/payslip/entity/Payslip.java
@@ -1,7 +1,6 @@
 package kr.co.awesomelead.groupware_backend.domain.payslip.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,15 +9,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
 import kr.co.awesomelead.groupware_backend.domain.user.entity.User;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payslip {
 
     @Id

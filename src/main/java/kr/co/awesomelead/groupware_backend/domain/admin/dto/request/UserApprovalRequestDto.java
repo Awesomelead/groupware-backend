8c4b6f79a397bb3daca34530165f4a5a7832bf32
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-12 15:25:10 +0900

    refactor: 직원이 최초 가입시 실수할 수 있는 필드들에 대해 관리자가 다시 작성할 수 있도록 필드 추가

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/admin/dto/request/UserApprovalRequestDto.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/admin/dto/request/UserApprovalRequestDto.java
@@ -1,31 +1,40 @@
 package kr.co.awesomelead.groupware_backend.domain.admin.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-
+import java.time.LocalDate;
 import kr.co.awesomelead.groupware_backend.domain.department.enums.Company;
-import kr.co.awesomelead.groupware_backend.domain.user.enums.Role;
-
+import kr.co.awesomelead.groupware_backend.domain.user.enums.JobType;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDate;
 
 @Getter
 @Setter
 public class UserApprovalRequestDto {
 
-    @Schema(description = "사용자 이름", example = "홍길동")
-    private LocalDate hireDate;
+    @Schema(description = "한글 이름 (수정 필요 시)", example = "홍길동")
+    private String nameKor;
+
+    @Schema(description = "영문 이름 (수정 필요 시)", example = "HONG GILDONG")
+    private String nameEng;
+
+    @Schema(description = "국적 (수정 필요 시)", example = "대한민국")
+    private String nationality;
+
+    @Schema(description = "전화번호 (수정 필요 시)", example = "01099998888")
+    private String phoneNumber;
+
+    @Schema(description = "부서 ID", example = "1")
+    private Long departmentId;
 
     @Schema(description = "직무 유형", example = "관리직")
-    private String jobType;
+    private JobType jobType;
 
     @Schema(description = "직급", example = "대리")
     private String position;
 
+    @Schema(description = "입사일", example = "2025-09-22")
+    private LocalDate hireDate;
+
     @Schema(description = "근무지", example = "AWESOME")
     private Company workLocation;
-
-    @Schema(description = "사용자 역할", example = "USER")
-    private Role role;
 }

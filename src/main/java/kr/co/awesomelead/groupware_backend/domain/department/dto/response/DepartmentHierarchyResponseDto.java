2c21caa24a8ab7bfb8339da766bc7d3f55508e6e
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-13 22:55:26 +0900

    refactor: String -> Enum에 따라 스키마 예시 수정

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/department/dto/response/DepartmentHierarchyResponseDto.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/department/dto/response/DepartmentHierarchyResponseDto.java
@@ -1,15 +1,12 @@
 package kr.co.awesomelead.groupware_backend.domain.department.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-
+import java.util.List;
 import kr.co.awesomelead.groupware_backend.domain.department.entity.Department;
 import kr.co.awesomelead.groupware_backend.domain.department.enums.DepartmentName;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.List;
 
 @Getter
 @Builder
@@ -19,7 +16,7 @@ public class DepartmentHierarchyResponseDto {
     @Schema(description = "부서 고유 ID", example = "1", required = true)
     private Long id;
 
-    @Schema(description = "부서명", example = "경영지원부", required = true)
+    @Schema(description = "부서명", example = "MANAGEMENT_SUPPORT", required = true)
     private DepartmentName name;
 
     @Schema(description = "하위 부서 목록 (재귀적 트리 구조)", required = true)
@@ -28,12 +25,12 @@ public class DepartmentHierarchyResponseDto {
     // 엔티티를 DTO로 변환하는 정적 팩토리 메서드
     public static DepartmentHierarchyResponseDto from(Department department) {
         return DepartmentHierarchyResponseDto.builder()
-                .id(department.getId())
-                .name(department.getName())
-                .children(
-                        department.getChildren().stream()
-                                .map(DepartmentHierarchyResponseDto::from) // 재귀 호출
-                                .toList())
-                .build();
+            .id(department.getId())
+            .name(department.getName())
+            .children(
+                department.getChildren().stream()
+                    .map(DepartmentHierarchyResponseDto::from) // 재귀 호출
+                    .toList())
+            .build();
     }
 }

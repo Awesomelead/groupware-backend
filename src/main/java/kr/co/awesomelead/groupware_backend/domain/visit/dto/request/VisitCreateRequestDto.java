7b628a5194442f4c755b07446fe257ee5842faa7
Go Yeongmin <ko07073@gmail.com>
2026-01-14 00:36:29 +0900

    fix: enum으로 변경됨에 따라 @Notblank -> @NotNull로 변경

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/dto/request/VisitCreateRequestDto.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/dto/request/VisitCreateRequestDto.java
@@ -1,22 +1,18 @@
 package kr.co.awesomelead.groupware_backend.domain.visit.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-
+import java.time.LocalDateTime;
+import java.util.List;
 import kr.co.awesomelead.groupware_backend.domain.department.enums.Company;
 import kr.co.awesomelead.groupware_backend.domain.visit.enums.VisitPurpose;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
@@ -42,17 +38,17 @@ public class VisitCreateRequestDto {
 
     @NotNull
     @Schema(
-            description = "방문 목적",
-            example = "MEETING",
-            required = true,
-            allowableValues = {
-                "CUSTOMER_INSPECTION",
-                "GOODS_DELIVERY",
-                "FACILITY_CONSTRUCTION",
-                "LOGISTICS",
-                "MEETING",
-                "OTHER"
-            })
+        description = "방문 목적",
+        example = "MEETING",
+        required = true,
+        allowableValues = {
+            "CUSTOMER_INSPECTION",
+            "GOODS_DELIVERY",
+            "FACILITY_CONSTRUCTION",
+            "LOGISTICS",
+            "MEETING",
+            "OTHER"
+        })
     private VisitPurpose purpose;
 
     @NotNull
@@ -63,7 +59,7 @@ public class VisitCreateRequestDto {
     @Schema(description = "담당자 ID", example = "1", required = true)
     private Long hostUserId;
 
-    @NotBlank
+    @NotNull
     @Schema(description = "담당자 회사명 (클라이언트에서 자동 주입)", example = "어썸리드", required = true)
     private Company hostCompany;
 
@@ -73,9 +69,9 @@ public class VisitCreateRequestDto {
 
     @Size(min = 4, max = 4, message = "비밀번호는 4자리여야 합니다.")
     @Schema(
-            description = "내방객 비밀번호 (4자리, 사전 예약 시 필수)",
-            example = "1234",
-            minLength = 4,
-            maxLength = 4)
+        description = "내방객 비밀번호 (4자리, 사전 예약 시 필수)",
+        example = "1234",
+        minLength = 4,
+        maxLength = 4)
     private String visitorPassword;
 }

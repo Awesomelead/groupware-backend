ddec9d1514485cbf223b501e511fdb5ef0b3b932
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-21 19:24:04 +0900

    feat: 내 방문정보에 대해 목록과 상세 조회용 DTO 구현

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/visit/dto/response/MyVisitListResponseDto.java
@@ -0,0 +1,33 @@
+package kr.co.awesomelead.groupware_backend.domain.visit.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDate;
+import kr.co.awesomelead.groupware_backend.domain.visit.enums.VisitStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@Schema(description = "내 방문 목록 조회 응답 (내방객용)")
+public class MyVisitListResponseDto {
+
+    @Schema(description = "방문 ID", example = "1")
+    private Long visitId;
+
+    @Schema(description = "내방객 이름", example = "홍길동")
+    private String visitorName;
+
+    @Schema(description = "방문 상태", example = "방문 전")
+    private VisitStatus status;
+
+    @Schema(description = "내방객 회사명", example = "어썸리드")
+    private String visitorCompany;
+
+    @Schema(description = "방문 시작일", example = "2024-07-01")
+    private LocalDate startDate;
+
+    @Schema(description = "방문 종료일", example = "2024-07-01")
+    private LocalDate endDate;
+}

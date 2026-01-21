b42f25b5be86fc0fde436a52b0b6015b36da3745
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-22 02:38:38 +0900

    feat: 방문에 대한 조회 및 퇴실처리를 권한으로 관리

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/user/enums/Authority.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/user/enums/Authority.java
@@ -5,5 +5,8 @@ public enum Authority {
     WRITE_MESSAGE, // 메세지 작성 권한 -> jobType이 관리직일 경우 부여하고 시작
     WRITE_EDUCATION, // 교육 작성 권한 -> jobType이 관리직일 경우 부여하고 시작
     WRITE_NOTICE, // 공지 작성 권한
+
+    ACCESS_VISIT, // 방문자 관리 시스템 접근 권한
+
     MANAGE_EMPLOYEE_DATA // 연차, 급여명세서, 근태확인표 발송 권한
 }

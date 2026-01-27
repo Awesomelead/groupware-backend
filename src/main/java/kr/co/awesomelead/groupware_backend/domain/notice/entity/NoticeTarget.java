920eda51fb1495e426d92c2066d2c425586c3066
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-28 03:00:34 +0900

    feat: 공지사항이 특정 직원들만을 대상으로 조회가 되는 시스템을 위한 중간 테이블 설계

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/notice/entity/NoticeTarget.java
@@ -0,0 +1,36 @@
+package kr.co.awesomelead.groupware_backend.domain.notice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import kr.co.awesomelead.groupware_backend.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "notice_targets")
+public class NoticeTarget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "notice_id")
+    private Notice notice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}

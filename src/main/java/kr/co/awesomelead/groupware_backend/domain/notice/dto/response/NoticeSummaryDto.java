e1d84f08322215f4f583ca8381823a208b27f967
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-27 20:52:43 +0900

    refactor: 공지 타입 없이 전체공지 반환을 위해 리스트 반환 DTO에도 공지 타입을 추가

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/notice/dto/response/NoticeSummaryDto.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/notice/dto/response/NoticeSummaryDto.java
@@ -1,13 +1,12 @@
 package kr.co.awesomelead.groupware_backend.domain.notice.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-
+import java.time.LocalDateTime;
+import kr.co.awesomelead.groupware_backend.domain.notice.enums.NoticeType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
@@ -18,6 +17,9 @@ public class NoticeSummaryDto {
 
     @Schema(description = "공지사항 ID", example = "1")
     private Long id;
+
+    @Schema(description = "공지사항 유형", example = "상시공지")
+    private NoticeType type;
 
     @Schema(description = "공지사항 제목", example = "2025년 1월 전체 회의 안내")
     private String title;

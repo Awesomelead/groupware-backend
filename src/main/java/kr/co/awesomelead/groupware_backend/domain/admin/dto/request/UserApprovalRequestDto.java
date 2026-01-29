5ce111020f24dc299ad3331b2e4d18bdfdfbcde3
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-30 00:42:27 +0900

    feat: 관리자의 직원 회원가입 승인시에 직원의 권한도 설정 가능하도록 수정 및 NotNull 어노테이션 추가

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/admin/dto/request/UserApprovalRequestDto.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/admin/dto/request/UserApprovalRequestDto.java
@@ -1,15 +1,14 @@
 package kr.co.awesomelead.groupware_backend.domain.admin.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
 import kr.co.awesomelead.groupware_backend.domain.department.enums.Company;
 import kr.co.awesomelead.groupware_backend.domain.user.enums.JobType;
 import kr.co.awesomelead.groupware_backend.domain.user.enums.Position;
-
+import kr.co.awesomelead.groupware_backend.domain.user.enums.Role;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -27,18 +26,26 @@ public class UserApprovalRequestDto {
     @Schema(description = "전화번호 (수정 필요 시)", example = "01099998888")
     private String phoneNumber;
 
+    @NotNull(message = "근무사업장은 필수 항목입니다.")
+    @Schema(description = "근무사업장", example = "어썸리드")
+    private Company workLocation;
+
+    @NotNull(message = "부서 ID는 필수 항목입니다.")
     @Schema(description = "부서 ID", example = "1")
     private Long departmentId;
 
-    @Schema(description = "직무 유형", example = "MANAGEMENT")
+    @NotNull(message = "직무 유형은 필수 항목입니다.")
+    @Schema(description = "직무 유형", example = "관리직")
     private JobType jobType;
 
-    @Schema(description = "직급", example = "ASSISTANT_MANAGER")
+    @Schema(description = "권한", example = "일반 사용자")
+    private Role role;
+
+    @NotNull(message = "직급은 필수 항목입니다.")
+    @Schema(description = "직급", example = "사원")
     private Position position;
 
+    @NotNull(message = "입사일은 필수 항목입니다.")
     @Schema(description = "입사일", example = "2025-09-22")
     private LocalDate hireDate;
-
-    @Schema(description = "근무지", example = "AWESOME")
-    private Company workLocation;
 }

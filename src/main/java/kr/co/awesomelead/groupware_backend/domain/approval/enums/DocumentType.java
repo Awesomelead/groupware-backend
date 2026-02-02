d34461c6ef35f3368ea89dbd7aea1641857bff2a
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-02-02 17:02:04 +0900

    feat: 전자결재 문서 종류를 한비로 요구사항에 맞게 추가 - 기본양식 - 근태신청서 - 국외출장여비정산서 - 기안및지출결의 및 복리후생 - 차량유류정산지출결의

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/approval/enums/DocumentType.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/approval/enums/DocumentType.java
@@ -1,13 +1,23 @@
 package kr.co.awesomelead.groupware_backend.domain.approval.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum DocumentType {
-    LEAVE_REQUEST("휴가신청서"),
-    MESSAGE("메시지문서");
+    BASIC("기본양식"),
+    LEAVE("근태신청서"),
+    OVERSEAS_TRIP("국외출장여비정산서"),
+    EXPENSE_DRAFT("기안및지출결의"),
+    WELFARE_EXPENSE("기안및지출결의_복리후생"),
+    CAR_FUEL("차량유류정산지출결의");
 
     private final String description;
+
+    @JsonValue
+    public String getDescription() {
+        return description;
+    }
 }

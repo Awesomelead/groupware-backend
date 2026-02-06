f9d03da41c1774f7b6e997a9481831b90de72084
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-02-06 17:09:51 +0900

    feat: 근태신청 필드 수정 - 기존 String leaveType에서 Enum값인 LeaveType, LeaveDetail로 구분 - LeaveType: '휴가, 반차, 교육, 훈련(예비군), 기타'로 나눠짐 - LeaveDetailType: '연차, 경조, 유급, 무급, 오전, 오후'로 나눠짐

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/approval/entity/document/LeaveApproval.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/approval/entity/document/LeaveApproval.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import java.time.LocalDateTime;
 import kr.co.awesomelead.groupware_backend.domain.approval.entity.Approval;
 import kr.co.awesomelead.groupware_backend.domain.approval.enums.DocumentType;
+import kr.co.awesomelead.groupware_backend.domain.approval.enums.LeaveDetailType;
+import kr.co.awesomelead.groupware_backend.domain.approval.enums.LeaveType;
 
 @Entity
 @DiscriminatorValue("LEAVE")
@@ -12,7 +14,8 @@ public class LeaveApproval extends Approval {
 
     private LocalDateTime startDate;
     private LocalDateTime endDate;
-    private String leaveType; // 구분: 연차, 반차 등
+    private LeaveType leaveType;
+    private LeaveDetailType leaveDetailType;
     private String reason;    // 신청 사유
     private String emergencyContact; // 비상 연락처
 

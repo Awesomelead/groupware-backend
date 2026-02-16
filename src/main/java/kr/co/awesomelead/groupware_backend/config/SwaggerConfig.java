e2d36b9c4dac566549d1b26df70fe9baa36da10c
Go Yeongmin <ko07073@gmail.com>
2026-02-16 18:30:03 +0900

    refactor: name 수정

--- src/main/java/kr/co/awesomelead/groupware_backend/config/SwaggerConfig.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/config/SwaggerConfig.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,27 +14,27 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        Info info = new Info().title("어썸그룹웨어 API").version("v1.0.0").description("어썸그룹웨어 API 입니다");
+        Info info = new Info().title("어썸리드 API").version("v1.0.0").description("어썸리드 API 입니다");
 
         // JWT 인증 스키마 정의
         SecurityScheme securityScheme =
-                new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")
-                        .in(SecurityScheme.In.HEADER)
-                        .name("Authorization");
+            new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
 
         // Security Requirement 정의
         SecurityRequirement securityRequirement =
-                new SecurityRequirement().addList("Bearer Authentication");
+            new SecurityRequirement().addList("Bearer Authentication");
 
         return new OpenAPI()
-                .components(
-                        new Components()
-                                .addSecuritySchemes("Bearer Authentication", securityScheme))
-                .addSecurityItem(securityRequirement)
-                .addServersItem(new Server().url("/"))
-                .info(info);
+            .components(
+                new Components()
+                    .addSecuritySchemes("Bearer Authentication", securityScheme))
+            .addSecurityItem(securityRequirement)
+            .addServersItem(new Server().url("/"))
+            .info(info);
     }
 }

c2133a200468a874e2b4f1ddd79ea0fea3db02b3
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-02-21 20:23:27 +0900

    feat: 전자 결재 상태에 '진행 중' 추가 - 결재라인에 있지 않은 유저들의 관점을 위함 - 결재 전이나 결재 대기가 아닌 결재가 진행 중임을 표현

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/approval/enums/ApprovalStatus.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/approval/enums/ApprovalStatus.java
@@ -10,6 +10,7 @@ import lombok.Getter;
 public enum ApprovalStatus {
     WAITING("대기"), // 앞 순서 결재자가 아직 승인을 안 해서 대기 중인 상태
     PENDING("승인 대기"), // 내 차례가 되어서 결재를 기다리는 상태 (알림 발송 대상)
+    IN_PROGRESS("진행 중"), // 다른 사람의 결재 차례일 때 (동적 상태 표출용)
     APPROVED("승인"), // 결재자가 승인함
     REJECTED("반려"), // 결재자가 반려함
     CANCELED("취소"); // 기안자가 상신을 취소함 (선택 사항)

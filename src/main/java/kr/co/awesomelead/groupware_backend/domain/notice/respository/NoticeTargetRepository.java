948d08cc45ed64ef3b240d51beb3054fe6e98866
Go Yeongmin <ko07073@gmail.com>
2026-02-24 23:58:06 +0900

    fix: 공지 삭제 시, 삭제하려는 ID의 noticeTarget도 같이 삭제

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/notice/respository/NoticeTargetRepository.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/notice/respository/NoticeTargetRepository.java
@@ -1,7 +1,14 @@
 package kr.co.awesomelead.groupware_backend.domain.notice.respository;
 
 import kr.co.awesomelead.groupware_backend.domain.notice.entity.NoticeTarget;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface NoticeTargetRepository extends JpaRepository<NoticeTarget, Long> {}
+public interface NoticeTargetRepository extends JpaRepository<NoticeTarget, Long> {
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("delete from NoticeTarget nt where nt.notice.id = :noticeId")
+    void deleteByNoticeId(@Param("noticeId") Long noticeId);
+}

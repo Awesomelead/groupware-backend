f55bd1a02b24ff423805cdc2d42e0a2466269128
Go Yeongmin <ko07073@gmail.com>
2026-02-26 16:49:44 +0900

    feat: 취소 상태 추가

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/user/enums/MyInfoUpdateRequestStatus.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/user/enums/MyInfoUpdateRequestStatus.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum MyInfoUpdateRequestStatus {
     PENDING("대기"),
     APPROVED("승인"),
-    REJECTED("반려");
+    REJECTED("반려"),
+    CANCELED("취소");
 
     private final String description;
 }

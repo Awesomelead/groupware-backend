82c653735c23d42b1dd439115a50c8b64631dec6
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-03-06 14:36:17 +0900

    feat: LoginResponseDto에 User.role 추가

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/auth/dto/response/LoginResponseDto.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/auth/dto/response/LoginResponseDto.java
@@ -1,9 +1,8 @@
 package kr.co.awesomelead.groupware_backend.domain.auth.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-
 import kr.co.awesomelead.groupware_backend.domain.user.enums.Position;
-
+import kr.co.awesomelead.groupware_backend.domain.user.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,15 +12,15 @@ import lombok.Getter;
 public class LoginResponseDto {
 
     @Schema(
-            description = "액세스 토큰 (Authorization 헤더에 'Bearer {token}' 형식으로 사용)",
-            example =
-                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAdGVzdC5jb20iLCJyb2xlIjoiUk9MRV9VU0VSIiwiaWF0IjoxNzM2NDA2MDAwLCJleHAiOjE3MzY0MDk2MDB9.signature")
+        description = "액세스 토큰 (Authorization 헤더에 'Bearer {token}' 형식으로 사용)",
+        example =
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAdGVzdC5jb20iLCJyb2xlIjoiUk9MRV9VU0VSIiwiaWF0IjoxNzM2NDA2MDAwLCJleHAiOjE3MzY0MDk2MDB9.signature")
     private String accessToken;
 
     @Schema(
-            description = "리프레쉬 토큰",
-            example =
-                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAdGVzdC5jb20iLCJyb2xlIjoiUk9MRV9VU0VSIiwiaWF0IjoxNzM2NDA2MDAwLCJleHAiOjE3MzY0MDk2MDB9.signature")
+        description = "리프레쉬 토큰",
+        example =
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAdGVzdC5jb20iLCJyb2xlIjoiUk9MRV9VU0VSIiwiaWF0IjoxNzM2NDA2MDAwLCJleHAiOjE3MzY0MDk2MDB9.signature")
     private String refreshToken;
 
     @Schema(description = "생성된 사용자 ID", example = "1")
@@ -35,4 +34,7 @@ public class LoginResponseDto {
 
     @Schema(description = "직급", example = "대리")
     private Position position;
+
+    @Schema(description = "역할", example = "관리자")
+    private Role role;
 }

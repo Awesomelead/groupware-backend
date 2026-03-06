15eb121896dfe489040527ce982f99de0e0ff100
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-03-07 01:41:03 +0900

    feat: 부서명을 통한 조회 JPA 추가

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/department/repository/DepartmentRepository.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/department/repository/DepartmentRepository.java
@@ -2,14 +2,18 @@ package kr.co.awesomelead.groupware_backend.domain.department.repository;
 
 import kr.co.awesomelead.groupware_backend.domain.department.entity.Department;
 import kr.co.awesomelead.groupware_backend.domain.department.enums.Company;
+import kr.co.awesomelead.groupware_backend.domain.department.enums.DepartmentName;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
     List<Department> findByParentIsNull();
 
     List<Department> findByParentIsNullAndCompany(Company company);
+
+    Optional<Department> findByName(DepartmentName name);
 }

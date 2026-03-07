2b4cf7eb0311b59ddf0d84543b7485b9d56e178c
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-03-07 18:52:45 +0900

    feat: Enum 리스트 응답값을 위한 DTO 구현

---
+++ src/main/java/kr/co/awesomelead/groupware_backend/global/common/dto/response/EnumCodeDto.java
@@ -0,0 +1,29 @@
+package kr.co.awesomelead.groupware_backend.global.common.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "공통 분류 코드 응답 객체")
+public class EnumCodeDto {
+
+    @Schema(description = "Enum 영문 상수 값", example = "CHUNGNAM_HQ")
+    private String code;
+
+    @Schema(description = "Enum 한글 설명문", example = "충남사업본부")
+    private String description;
+
+    public static EnumCodeDto of(String code, String description) {
+        return EnumCodeDto.builder()
+                .code(code)
+                .description(description)
+                .build();
+    }
+}

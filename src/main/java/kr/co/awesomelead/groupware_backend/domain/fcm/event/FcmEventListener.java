905fdbd99261bb469f53ee7fef2da3d7a71c9483
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-03-22 22:26:24 +0900

    feat: sendToUser의 FCM 예외처리에 따라 throws 작성

--- src/main/java/kr/co/awesomelead/groupware_backend/domain/fcm/event/FcmEventListener.java
+++ src/main/java/kr/co/awesomelead/groupware_backend/domain/fcm/event/FcmEventListener.java
@@ -1,5 +1,7 @@
 package kr.co.awesomelead.groupware_backend.domain.fcm.event;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
+
 import kr.co.awesomelead.groupware_backend.domain.fcm.service.FcmService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,7 +19,7 @@ public class FcmEventListener {
     private final FcmService fcmService;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleFcmSendEvent(FcmSendEvent event) {
+    public void handleFcmSendEvent(FcmSendEvent event) throws FirebaseMessagingException {
         log.debug("FCM 이벤트 수신 (AFTER_COMMIT) - userId: {}", event.userId());
         fcmService.sendToUser(event.userId(), event.title(), event.body(), event.data());
     }
